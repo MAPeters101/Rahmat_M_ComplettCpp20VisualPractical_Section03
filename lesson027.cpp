21dc23742e96bb56d35f5a9976a6adaea563ac1c
Mark Peters <mapeters101@earthlink.net>
2024-12-02 12:01:40 -0500

    Work on lesson027.

---
+++ lesson027.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+/* 
+int main() {
+	int SALES_TAX_RATE{ 8 };
+	int Quantity{ 0 };
+	int Price{ 0 };
+
+	cout << "Enter the quantity: ";
+	cin >> Quantity;
+
+	cout << "Enter the price per item: ";
+	cin >> Price;
+
+	int SubTotal = Quantity * Price;
+
+	int SalesTax = (SubTotal * SALES_TAX_RATE) / 100;
+
+	int TotalCost = SubTotal + SalesTax;
+
+	cout << "Subtotal: $" << SubTotal << endl;
+	cout << "Sales Tax: $" << SalesTax << endl;
+	cout << "Total Cost: $" << TotalCost << endl;
+
+
+	return 0;
+}
+*/
+
