bc204237906b16e4fbb7bdb62da9dd902eb1fc04
Mark Peters <mapeters101@earthlink.net>
2024-12-01 20:53:10 -0500

    Work on lesson024.

---
+++ lesson024.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+int main() {
+	int Ammo{ 30 };
+	cout << Ammo << endl;
+	Ammo++;
+	cout << Ammo << endl;
+	Ammo--;
+	cout << Ammo << endl;
+	cout << "\n\n";
+
+
+	cout << Ammo++ << endl;
+	cout << Ammo << endl;
+	cout << ++Ammo << endl;
+	cout << Ammo << endl;
+	cout << Ammo-- << endl;
+	cout << Ammo << endl;
+	cout << --Ammo << endl;
+	cout << Ammo << endl;
+
+
+	return 0;
+}
+*/
+
+
+
