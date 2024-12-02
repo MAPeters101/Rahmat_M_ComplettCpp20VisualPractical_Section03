757dd6a44660c7ed33913fbaab4f68d5ab7b865a
Mark Peters <mapeters101@earthlink.net>
2024-12-02 13:42:16 -0500

    Work on lesson030.

---
+++ lesson030.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+
+using namespace std;
+
+/*
+int main() {
+	char CharValue{ 'C' };
+	cout << "Character Value: " << CharValue << endl;
+
+	//wchar_t nlTheta{ 'θ' };
+	//wcout << "Theta Value: " << nlTheta << endl;
+
+	//wchar_t Theta{ L'θ' };
+	//wcout << "Theta Value: " << Theta << endl;
+
+
+	char16_t Dell_ta{ u'Δ' };
+	wcout << "Dell_ta Value: " << Dell_ta << endl;
+
+	char32_t SmilingFace{ U'🙂' };
+	wcout << "SmilingFace Value: " << SmilingFace << endl;
+
+	char32_t SmilingFaceH{ U'\U0001F642' };
+	wcout << "SmilingFaceH Value: " << SmilingFaceH << endl;
+
+	//char8_t Yen{ u'¥' };
+
+	return 0;
+}
+*/
+
+
