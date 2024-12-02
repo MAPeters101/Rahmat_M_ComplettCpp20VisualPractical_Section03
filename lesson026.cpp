3c359fc73337c58dfbabfca7db77ae865971da06
Mark Peters <mapeters101@earthlink.net>
2024-12-02 11:52:42 -0500

    Work on lesson026.

---
+++ lesson026.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+/*
+int main() {
+	const double SALES_TAX_RATE{ 0.08 };
+	int Quantity{ 0 };
+	double Price{ 0 };
+	cout << "Please enter the number of items to buy: ";
+	cin >> Quantity;
+	cout << "Please enter the cost per item: ";
+	cin >> Price;
+	double Subtotal{ Quantity * Price };
+	double SalesTax{ Subtotal * SALES_TAX_RATE };
+	double TotalCost{ Subtotal + SalesTax };
+	cout << "Subtotal: $" << Subtotal << ", Sales Tax: $" << SalesTax << ", Total Cost: $" << TotalCost << "." << endl;
+	
+
+	return 0;
+}
+*/
+
