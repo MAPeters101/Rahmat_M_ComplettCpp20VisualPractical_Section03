5048a8b70beafc78249ea688626d6d1d83453876
Mark Peters <mapeters101@earthlink.net>
2024-12-01 20:17:00 -0500

    Work on lesson021.

---
+++ lesson021.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+int main() {
+	short int Short{ 19 };
+	//short int Short{ 19345234523452 };
+	int IntNumber{ 20 };
+	long int Long{ 24 };
+	long long int VeryLong{ 4524234 };
+
+	cout << Short << endl;
+	cout << IntNumber << endl;
+	cout << Long << endl;
+	cout << VeryLong << endl;
+	cout << "====================\n\n";
+
+
+	short int nShort{ -19 };
+	int nIntNumber{ -20 };
+	long int nLong{ -24 };
+	long long int nVeryLong{ -4524234 };
+
+	cout << nShort << endl;
+	cout << nIntNumber << endl;
+	cout << nLong << endl;
+	cout << nVeryLong << endl;
+	cout << "====================\n\n";
+
+	unsigned short int uShort{ 19 };
+	unsigned int uIntNumber{ 20 };
+	unsigned long int uLong{ 24 };
+	unsigned long long int uVeryLong{ 4524234 };
+
+	cout << uShort << endl;
+	cout << uIntNumber << endl;
+	cout << uLong << endl;
+	cout << uVeryLong << endl;
+	cout << "====================\n\n";
+
+	return 0;
+}
+*/
+
+
