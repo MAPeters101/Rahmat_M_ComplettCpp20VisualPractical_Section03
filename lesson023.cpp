826416956598563112b8e5deee2aeb458c38ae0e
Mark Peters <mapeters101@earthlink.net>
2024-12-01 20:46:23 -0500

    Complete lesson023.

--- lesson023.cpp
+++ lesson023.cpp
@@ -2,20 +2,27 @@
 
 using namespace std;
 
+/*
 int main() {
-	int NumItem{ 25 };
-	int PricePerItem{ 3 };
-	int TotalCost;
+	int NumItem{ 0 };
+	const int PricePerItem{ 3 };
+	int TotalCost{ 0 };
+
+	cout << "Enter number of items: ";
+	cin >> NumItem;
 
 	TotalCost = NumItem * PricePerItem;
 	cout << "The total cost of " << NumItem << " items at $" << PricePerItem << " each is $" << TotalCost << "." << endl;
 
-	int AmountPaid{ 100 };
-	int Change;
+	int AmountPaid{ 0 };
+	cout << "Enter the amount paid: ";
+	cin >> AmountPaid;
+
+	int Change{ 0 };
 	Change = AmountPaid - TotalCost;
 
 	cout << "The amount paid was $" << AmountPaid << ", and the change due is $" << Change << "." << endl;
 	return 0;
 }
-
+*/
 
