403ac1a7b54d45534e4b0f6c1298eb1eef1f386a
Mark Peters <mapeters101@earthlink.net>
2024-12-02 13:55:12 -0500

    Work on lesson031 to 14:00.

---
+++ lesson031.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+	string Name{ "John" };
+	cout << Name << endl;
+	cout << "-----------------------------\n\n";
+
+	string NameM{ "Alice" };
+	int Age{ 22 };
+	cout << "My name is " << NameM << ", I am " << Age << " years old.\n\n";
+
+	return 0;
+}
+
+
