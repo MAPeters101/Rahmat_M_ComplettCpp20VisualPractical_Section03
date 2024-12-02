cfd9f9e5e99c6ff6c3f16e3896869e011faafd09
Mark Peters <mapeters101@earthlink.net>
2024-12-01 21:00:30 -0500

    Work on lesson025.

---
+++ lesson025.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+int main() {
+	short ShortNum{ 32 };
+	int IntNum{ 2342 };
+	long LongNum{ 34534 };
+	long long VeryLong{ 2325234 };
+
+	unsigned short uShortNum{ 32 };
+	unsigned int uIntNum{ 2342 };
+	unsigned long uLongNum{ 34534 };
+	unsigned long long uVeryLong{ 2325234 };
+
+	cout << sizeof(ShortNum) << endl;
+	cout << sizeof(IntNum) << endl;
+	cout << sizeof(LongNum) << endl;
+	cout << sizeof(VeryLong) << endl;
+	cout << "==================\n\n";
+
+	cout << sizeof(uShortNum) << endl;
+	cout << sizeof(uIntNum) << endl;
+	cout << sizeof(uLongNum) << endl;
+	cout << sizeof(uVeryLong) << endl;
+
+	return 0;
+}
+*/
+
