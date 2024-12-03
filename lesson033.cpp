70acc0cc05e5ce9a8104688c359711ff3d3e3430
Mark Peters <mapeters101@earthlink.net>
2024-12-03 16:53:11 -0500

    Complete lesson033.

--- lesson033.cpp
+++ lesson033.cpp
@@ -2,9 +2,18 @@
 
 using namespace std;
 
+const double PI{ 3.14 };
+
+void Example() {
+	//cout << LocalToMain << endl;
+	cout << PI << endl;
+}
+
+/*
 int main() {
 	int LocalToMain{ 2342 };
 	cout << LocalToMain << endl;
+	cout << PI << endl;
 
 	//int LocalToMain{ 11 };
 
@@ -14,15 +23,15 @@ int main() {
 		cout << LocalToMain << endl;
 		int LocalToMain{ 11 };
 		cout << LocalToMain << endl;
+		cout << PI << endl;
 
 	}
 	//cout << LocalToInsideMain << endl;
 	cout << LocalToMain << endl;
 
+	Example();
+
 	return 0;
 }
-
-void Example() {
-	//cout << LocalToMain << endl;
-}
+*/
 
