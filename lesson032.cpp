81ddbfa380aa00d1b166458ca0e37bb9c2c2d7c6
Mark Peters <mapeters101@earthlink.net>
2024-12-03 16:39:03 -0500

    Work on lesson032.

---
+++ lesson032.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+int main() {
+	auto Number{ 1234 };
+	auto String{ "Name" };
+	auto PI{ 3.14159 };
+	auto Character{ 'C' };
+	auto MyName{ String };
+
+	cout << "Int Value: " << Number << endl;
+	cout << "String Value: " << String << endl;
+	cout << "Pi Value: " << PI << endl;
+	cout << "Character Value: " << Character << endl;
+	cout << "MyName Value: " << MyName << endl;
+
+	return 0;
+}
+*/
+
