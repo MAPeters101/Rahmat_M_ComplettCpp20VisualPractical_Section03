0aa7ddae768f401ae3a962a170f58872da603bd8
Mark Peters <mapeters101@earthlink.net>
2024-12-03 16:17:54 -0500

    Work on lesson031 to 22:00.

--- lesson031.cpp
+++ lesson031.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <string>
+#include <format>
 
 using namespace std;
 
@@ -11,6 +13,27 @@ int main() {
 	string NameM{ "Alice" };
 	int Age{ 22 };
 	cout << "My name is " << NameM << ", I am " << Age << " years old.\n\n";
+	cout << "-----------------------------\n\n";
+
+	string NameF{ "Finn" };
+	int AgeF{ 28 };
+	string FormattedText{ std::format("My Name is {} and I am {} years old.", NameF, AgeF) };
+	cout << FormattedText << endl;
+	cout << "-----------------------------\n\n";
+
+	int X{ 32 };
+	string FormattedString{ format("{:04}", X) };
+	cout << FormattedString << endl;
+	cout << "-----------------------------\n\n";
+
+	double PI{ 3.14159265 };
+	string FormattedDouble{ format("{:.2}", PI) };
+	cout << FormattedDouble << endl;
+
+	string FormattedDouble1{ format("{:.2f}", PI) };
+	cout << FormattedDouble1 << endl;
+
+
 
 	return 0;
 }
