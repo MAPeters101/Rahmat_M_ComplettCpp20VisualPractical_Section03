cde127e4ae8d4e0c2a841ae0e3dbe76f235fc196
Mark Peters <mapeters101@earthlink.net>
2024-12-04 14:30:03 -0500

    Work on lesson035.

---
+++ lesson035.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <format>
+
+using namespace std;
+
+/*
+int main() {
+	int Num1{ 0 }, Num2{ 0 }, Num3{ 0 };
+	cout << "Please enter the first number: ";
+	cin >> Num1;
+	cout << "Please enter the second number: ";
+	cin >> Num2;
+	cout << "Please enter the third number: ";
+	cin >> Num3;
+	double Sum{ static_cast<double>(Num1) + static_cast<double>(Num2) + static_cast<double>(Num3)};
+	double Average{ Sum / 3.0 };
+	cout << format("The sum of {}, {}, {} is {}, and the average is {}.", Num1, Num2, Num3, Sum, Average);
+	return 0;
+}
+*/
+
