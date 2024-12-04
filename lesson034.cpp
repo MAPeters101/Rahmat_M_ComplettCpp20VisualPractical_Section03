59e7fc62bd4b77dad0537861607f4c62547ba98f
Mark Peters <mapeters101@earthlink.net>
2024-12-04 14:20:57 -0500

    Complete work on lesson034.

--- lesson034.cpp
+++ lesson034.cpp
@@ -1,15 +1,16 @@
 #include <iostream>
 #include <climits>
 #include <cfloat>
-
+#include <format>
 
 using namespace std;
 
+/*
 int main() {
 	int SizeOfVariable{ 42 };
 	cout << "Size of the variable: " << sizeof(SizeOfVariable) << endl;
 	cout << "Size of variable data type: " << sizeof(int) << endl;
-	cout << "-------------------\n\n";
+	cout << "---------------------------------------\n\n";
 
 	cout << "Short Min: \t" << SHRT_MIN << endl;
 	cout << "Short Max: \t" << SHRT_MAX << endl;
@@ -23,11 +24,20 @@ int main() {
 	cout << "Float Max: \t" << FLT_MAX << endl;
 	cout << "Char Min: \t" << CHAR_MIN << endl;
 	cout << "Char Max: \t" << CHAR_MAX << endl;
+	cout << "---------------------------------------\n\n\n\n";
 
 
-
-
+	cout << "-----------------------------------------------------------------\n";
+	cout << format("| Variable: \t| Min \t\t\t| Max\t\t\t|") << endl;
+	cout << "-----------------------------------------------------------------\n";
+	cout << format("| Short: \t| {} \t\t| {}\t\t\t|", SHRT_MIN, SHRT_MAX) << endl;
+	cout << format("| Int: \t\t| {} \t\t| {}\t\t|", INT_MIN, INT_MAX) << endl;
+	cout << format("| Long: \t| {} \t\t| {}\t\t|", LONG_MIN, LONG_MAX) << endl;
+	cout << format("| LLong: \t| {} \t| {}\t|", LLONG_MIN, LLONG_MAX) << endl;
+	cout << format("| Float: \t| {} \t| {}\t\t|", FLT_MIN, FLT_MAX) << endl;
+	cout << format("| Char: \t| {} \t\t\t| {}\t\t\t|", CHAR_MIN, CHAR_MAX) << endl;
+	cout << "-----------------------------------------------------------------\n";
 
 	return 0;
 }
-
+*/
