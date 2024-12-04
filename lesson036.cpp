04d9a2ae206f86766cc5aa6d529aad52b80c5af5
Mark Peters <mapeters101@earthlink.net>
2024-12-04 14:37:47 -0500

    Work on lesson036.

---
+++ lesson036.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+int main() {
+	int Num1{ 0 }, Num2{ 0 }, Num3{};
+
+	cout << "Enter the first number: ";
+	cin >> Num1;
+	cout << "Enter the second number: ";
+	cin >> Num2;
+	cout << "Enter the third number: ";
+	cin >> Num3;
+
+	double Sum = static_cast<double>(Num1) + Num2 + Num3;
+	double Average = Sum / 3.0;
+
+	cout << "The sum of the three numbers is: " << Sum << endl;
+	cout << "The average of the three numbers is: " << Average << endl;
+
+	return 0;
+}
+*/
+
+
